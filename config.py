098cbd5775bb001d542e6bc9d87711c11dd9b3da
BuildTools <the.mathboy19@gmail.com>
2015-04-12 20:17:17 -0500

    added easy files

---
+++ config.py
@@ -0,0 +1,24 @@
+worlds["ams"] = "/home/mathboy19/programs/mcmap/world"
+
+renders["amsday"] = {
+    "world": "ams",
+    "title": "AMS Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld"
+}
+
+#renders["amsnether"] = {
+#    "world": "ams",
+#    "title": "AMS Nether",
+#    "rendermode": nether_smooth_lighting,
+#    "dimension": "nether"
+#}
+
+#renders["amscaves"] = {
+#    "world": "ams",
+#    "title": "AMS Caves",
+#    "rendermode": cave,
+#    "dimension": "overworld"
+#}
+
+outputdir = "/home/mathboy19/programs/mcmap"
---
+++ overviewerConfig.js
@@ -0,0 +1,65 @@
+var overviewerConfig = {
+    "worlds": [
+        "ams - overworld"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                194, 
+                80, 
+                201
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1428884215, 
+            "name": "AMS Daytime", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 10, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "ams - overworld", 
+            "maxZoom": 10, 
+            "path": "amsday", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1428886075"
+    }
+};
